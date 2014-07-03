9af7836c6a4934fc848599470a6a8dfb9f563195
Jessica Tatham <jectatham@gmail.com>
2014-07-03 19:02:17 -0400

    WIP making the view for adventures to test database functionality

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,26 +1,26 @@
-module ApplicationHelper
-	user_location
-		geo = Geocoder.search(request.remote_ip).first
-		return [geo.latitude,geo.longitude]
-	end
+# module ApplicationHelper
+# 	user_location
+# 		geo = Geocoder.search(request.remote_ip).first
+# 		return [geo.latitude,geo.longitude]
+# 	end
 
-	coords_from_address(address)
-		geo = Geocoder.search(address).first
-		return [geo.latitude,geo.longitude]
-	end
+# 	coords_from_address(address)
+# 		geo = Geocoder.search(address).first
+# 		return [geo.latitude,geo.longitude]
+# 	end
 
-	address_from_coords(latitude,longitude)
-		geo = Geocoder.search([latitude,longitude]).first
-		geo.formatted_address
-	end
+# 	address_from_coords(latitude,longitude)
+# 		geo = Geocoder.search([latitude,longitude]).first
+# 		geo.formatted_address
+# 	end
 
-	city_from_coords(latitude,longitude)
-		geo = Geocoder.search([latitude,longitude]).first
-		geo.city
-	end
+# 	city_from_coords(latitude,longitude)
+# 		geo = Geocoder.search([latitude,longitude]).first
+# 		geo.city
+# 	end
 
-	country_from_coords(latitude,longitude)
-		geo = Geocoder.search([latitude,longitude]).first
-		geo.country
-	end
-end
+# 	country_from_coords(latitude,longitude)
+# 		geo = Geocoder.search([latitude,longitude]).first
+# 		geo.country
+# 	end
+# end
