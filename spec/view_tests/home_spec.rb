992c0edd91a9655b148d3e623dd690b833e47db2
DineshRai <drai89@gmail.com>
2014-07-03 18:52:47 -0400

    add helper methods

---
+++ spec/view_tests/home_spec.rb
@@ -0,0 +1,8 @@
+describe 'user/index.html.erb' do 
+	it 'displays three buttons' do 
+		visit user_index
+		expect(page).to have_content 'local'
+		expect(page).to have_content 'continental'
+		expect(page).to have_content 'global'
+	end
+end
