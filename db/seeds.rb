68614289e0f657361c93323516ec15382f9b623e
Jessica Tatham <jectatham@gmail.com>
2014-07-04 18:02:42 -0400

    Seeding top 20 adventures and g adventure tours into database complete. Need to seed images, Lasse do this

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,63 +20,61 @@ end
 # G Adventure Tours
 def gtours
   tours = []
-  # @data_countries = []
-  # @tours_countries = []
+  @data_countries = []
+  @tours_countries = []
 
   CSV.foreach('db/gtours.csv') do |row|
     tours << row
   end
 
-  # tours.each do |t|
-  #   #countries
-  #   countries = t[8].gsub('[','').gsub(']','').gsub(' ','').gsub('"','').split(',')
-  #   @tours_countries << countries
-  #   @data_countries << countries
-  # end
-
-  # @data_countries = @data_countries.flatten.uniq
-  # @data_countries.each do |country|
-  #   c = Country.create(name: country)
-  # end
-
-  # tours.each do |t|
-
-  #   #cities
-  #   country = Country.find_by(name:t[7])
-  #   city = City.find_or_create_by(name: t[4], country_id: country.id)
-  #   p city
-
-  #   # # #adventure
-  #   adventure = Adventure.find_or_create_by(name:t[0], duration:t[1],description:t[2],city_id: city.id, map_url: t[5])
-  #   p adventure
-
-  # end
-  # i = 0
-  # tours.each do |t|
-  #   @tours_countries[i].each do |country|
-  #     country_adventure = CountryAdventure.create(country:Country.find_by(name: country), adventure: Adventure.find_by(name:t[0]))
-  #     p country_adventure
-  #   end
-  #   i += 1
-  # end
-    # #category
-    tours.each do |t|
-      categories = t[3].gsub('[','').gsub(']','').gsub(' ','').gsub('"','').split(',')
-      Category.find_or_create_by(name:t[3])
-
-      # CategoryAdventure.create
-    end
-  #   #image
-
-  #   if image = Image.find_by(url: t[6])
-  #     image
-  #   else
-  #     image = Image.create(url: t[6])
-  #     adventure.images << image
-  #     country.images << image
-  #     city.images << image
-  #   end
-  # end
+#countries
+tours.each do |t|
+  countries = t[8].gsub('[','').gsub(']','').gsub(' ','').gsub('"','').split(',')
+  @tours_countries << countries
+  @data_countries << countries
 end
 
+@data_countries = @data_countries.flatten.uniq
+@data_countries.each do |country|
+  c = Country.create(name: country)
+end
+
+#cities
+tours.each do |t|
+  country = Country.find_by(name:t[7])
+  city = City.find_or_create_by(name: t[4], country_id: country.id)  
+
+#adventure
+  adventure = Adventure.find_or_create_by(name:t[0], duration:t[1],description:t[2],city_id: city.id, map_url: t[5])
+  p adventure
+end
+#country adventure
+i = 0
+tours.each do |t|
+  @tours_countries[i].each do |country|
+    country_adventure = CountryAdventure.create(country:Country.find_by(name: country), adventure: Adventure.find_by(name:t[0]))
+  end
+  i += 1
+end
+#category and category adventure
+tours.each do |t|
+  categories = t[3].gsub('[','').gsub(']','').gsub(' ','').gsub('"','').split(',')
+  categories.each do |cat|
+    category = Category.find_or_create_by(name:cat) unless cat.match(/\d/)
+    catad = CategoryAdventure.create(category: category, adventure: Adventure.find_by(name:t[0]))
+  end
+end
+#image
+# tours.each do |t|
+#   image = Image.find_or_create_by(url: t[6])
+#   adventure = Adventure.find_by(name: t[0])
+#   country = Country.find_by(name: t[7])
+#   city = City.find_by(name:t[4])
+#   adventure.images << image
+#   country.images << image
+#   city.images << image
+# end
+end
+
+top20
 gtours
