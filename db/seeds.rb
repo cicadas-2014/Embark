0c0a49f67b794c6d82fa7d0d0db193bd5c0daf5d
Jason Matney <jamatney@gmail.com>
2014-07-04 12:05:10 -0400

    added database_schema.png and index.html d3 code

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'net/http'
+
+url = "https://rest.gadventures.com/tours"
+credentials = "#{"X-Application-Key"}:#{live_0ed49b6452ad05041e52d88b6893bf0f4d0a7b7e}"
+RestClient.post "https://credentials@#{url}", '<hello xmlns="https://rest.gadventures.com/tours"/>'
+
+
+# url = 'https://rest.gadventures.com/tours'
+# mykey = 'live_0ed49b6452ad05041e52d88b6893bf0f4d0a7b7e'
+# uri = URI.parse(url)
+
+# request = Net::HTTP.get_response(uri).body
+# p request
+# request['X-Application-Key'] = mykey
+
+# response = Net::HTTP.new(uri.host,uri.port) do |http|
+#   http.request(request)
+# end
+
+# p response.inspect
