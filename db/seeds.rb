5b7c4151a9a32498e65310351c9a515ec616f213
Jessica Tatham <jectatham@gmail.com>
2014-07-04 23:01:16 -0400

    working adventure display page using seed photos

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,10 +10,10 @@ def top20
 
   sliced = adventures.each_slice(5)
   sliced.each do |s|
-    @adventure = Adventure.create(name: s[0][0], duration: s[2][0].to_i, description: s[3][0])
-    @country = Country.create(name: s[1][0])
-    CountryAdventure.create(country_id: @country.id, adventure_id: @adventure.id)
-    Image.create(url: s[4][0], adventure_id: @adventure.id)
+    adventure = Adventure.create(name: s[0][0], duration: s[2][0].to_i, description: s[3][0])
+    country = Country.create(name: s[1][0])
+    CountryAdventure.create(country: country, adventure: adventure)
+    image = Image.create(url: s[4][0])
   end
 end
 
