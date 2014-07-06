e86653cdd30aa305960f9f6f79c98b94f82f5178
Jason Matney <jamatney@gmail.com>
2014-07-05 21:06:37 -0400

    worked on some specs

--- spec/features/description_spec.rb
+++ spec/features/description_spec.rb
@@ -1,13 +1,15 @@
 require 'rails_helper'
 
-feature 'Home page' do
-
-	scenario 'visit page' do
-		description = create(:description)
-		visit home_path
-		expect(page).to have_content('Adventure Description')
-		expect(page).to have_content(description.content)
-		expect(page).to have_content(desctiption.name)
-		expect(page).to have_content(desctiption.title)
+describe 'views/embark/description.html.erb' do	
+	context 'description' do
+		it "description " do
+			visit home_path
+			expect(page).to have_selector("a", text: "Local")
+		end
+		it "description " do
+			visit home_path
+			click_link("Local")
+			expect(current_path).to eq adventures_path
+		end
 	end
 end
--- spec/models/country_spec.rb
+++ spec/models/country_spec.rb
@@ -2,22 +2,22 @@ require 'rails_helper'
 
 describe Country do
 
-  let(:country) { build(:country) }
+  let(:country) { Country.create({name: "Jarkata"}) }
 
   it "should be vaild" do
     expect(country).to be_valid
   end
 
   it "is invalid without a name" do
-    expect(build(:country, name: nil)).to have(1).errors_on(:name)
+    expect(Country.create({ name: nil})).to eq(1).errors_on(:name)
   end
 
   it "is invalid without a description" do
-    expect(build(:country, description:nil)).to have(1).errors_on(:description)
+    expect(build(:country, description:nil)).to eq(1).errors_on(:description)
   end
 
   it "cannot have a duplicate name" do
     create(:country, name: "Argentina")
-    expect(build(:country, name: "Argentina")).to have(1).errors_on(:name)
+    expect(build(:country, name: "Argentina")).to eq(1).errors_on(:name)
   end
 end
