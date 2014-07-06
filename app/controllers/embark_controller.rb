a982a091b826b8601a5d3111b1f9e894081eee29
Lasse Sviland <lassesviland@hotmail.com>
2014-07-06 00:21:49 -0400

    added filtering based on location

--- app/controllers/embark_controller.rb
+++ app/controllers/embark_controller.rb
@@ -1,17 +1,34 @@
 class EmbarkController < ApplicationController
 
-def home
+	def home
 
-end
+	end
 
-def adventures
-  @categories = Category.all
-  all_adventures = Adventure.all
-  @adventures = all_adventures.sample(21)
-end
+	def adventures
+		geo = Geocoder.search(request.remote_ip).first
+		location = [geo.latitude,geo.longitude] 
 
-def description
-end
+		distance = 4 if params[:search] == 'local'
+		distance = 16 if params[:search] == 'continential'
+		distance = 1000000 if params[:search] == 'global'
+
+		valid_adventures = []
+		all_adventures = Adventure.all
+		all_adventures.each do |adv|
+			if adv.city_id
+				city = City.find(adv.city_id)
+				possition = [city.latitude, city.longitude]
+				if possition[0].to_i - location[0].to_i+distance < distance*2 && possition[0].to_i - location[1].to_i+distance < distance*2
+					valid_adventures << adv
+				end
+			end
+		end
+		@categories = Category.all
+		@adventures = valid_adventures.sample(27)
+	end
+
+	def description
+	end
 
 
 end
