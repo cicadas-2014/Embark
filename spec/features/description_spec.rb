f7d7696fc041c58a1205b65024a4579bcc7f400d
Lasse Sviland <lassesviland@hotmail.com>
2014-07-06 18:57:51 -0400

    Fixed broken feature test for description page

--- spec/features/description_spec.rb
+++ spec/features/description_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe "adventure" do
 		let!(:adventure) { Adventure.create(id: 31, name: 'adventure', description: 'description', duration: 2, map_url: 'map.png', image_url: 'image.png') }
 		scenario "the page should have the adventure's image" do
 			visit '/adventures/31'
-		expect(page).to have_css('#show_images')
+		expect(page).to have_css('.show_images')
 	end
 end
 	feature 'views/embark/description.html.erb' do	
