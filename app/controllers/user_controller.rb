d0f7389e1b88b91db2d4ff1abfb11ea4503a5c76
DineshRai <drai89@gmail.com>
2014-07-07 19:51:42 -0400

    description page design

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,6 +1,7 @@
 class UserController < ApplicationController
-	def index
-	end
+  def login
+  end
+
+  def signup
+  end
 end
-
-
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'user/login'
+
+  get 'user/signup'
+
 root 'embark#home', as: 'home'
 get '/adventures', to: 'embark#adventures', as: 'adventures'
 get '/adventures/:adventure_id', to: 'embark#description', as: 'description'
