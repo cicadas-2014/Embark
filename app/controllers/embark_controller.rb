f52929a8f585ce8599b684f28fc7418e8a2b0d9d
Lasse Sviland <lassesviland@hotmail.com>
2014-07-07 02:05:50 -0400

    Added debugging for heroku

--- app/controllers/embark_controller.rb
+++ app/controllers/embark_controller.rb
@@ -17,6 +17,8 @@ class EmbarkController < ApplicationController
 		valid_adventures = []
 		all_adventures = Adventure.all
 		all_adventures.each do |adv|
+			puts '*'*50
+			puts adv.id
 			if adv.city_id
 				city = City.find(adv.city_id)
 				position = [city.latitude, city.longitude]
