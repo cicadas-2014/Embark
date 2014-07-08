8c228fada4b343a689dee433d3df2dfe079c73bc
DineshRai <drai89@gmail.com>
2014-07-07 21:29:41 -0400

    add login and signup buttons on home page that will append signup and login forms

--- app/assets/javascripts/authentication.js
+++ app/assets/javascripts/authentication.js
@@ -1,10 +1,22 @@
-var loginForm = "<form method='post' action='user#login'><p><input type='text' name='email' placeholder= 'Email'></p><p><input type='password' name='password' value='password'></p><p class='submit'><input type='submit' value='Submit'></p></form>";
+var loginForm = "<form method='post' action='user#login'><p><input type='text' name='email' placeholder= 'Email'></p><p><input type='password' name='password'></p><p class='submit'><input type='submit' value='Login to your account'></p></form>";
+
+var signupForm = "<form method='post' action='user#signup'><p><input type='text' name='email' placeholder= 'Email'></p><p><input type='password' name='password'></p><p class='submit'><input type='submit' value='Create Account'></p></form>";
+
+var hideButtons = function() {
+  $('#signup').hide();
+  $('#login').hide();
+}
 
 var appendLoginForm = function(e) {
   e.preventDefault();
-  $('#signup').hide();
-  $('#login').hide();
+  hideButtons();
   $('#new_session').append(loginForm);
+}
+
+var appendSignupForm = function(e) {
+  e.preventDefault();
+  hideButtons();
+  $('#new_session').append(signupForm);
 }
 
 
@@ -16,9 +28,7 @@ var appendLoginForm = function(e) {
 
 
 
-
-
 $(document).ready(function() {
-  $('body').on('click', '#login', appendLoginForm)
-
+  $('body').on('click', '#login', appendLoginForm);
+  $('body').on('click', '#signup', appendSignupForm);
 })
