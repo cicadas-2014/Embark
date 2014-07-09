99c0d0ff881b819417468d798226ee4eb6fa1b89
Jessica Tatham <jectatham@gmail.com>
2014-07-09 12:50:09 -0400

    Working javascript to allow inspire me button to popup

--- app/assets/javascripts/adventure.js
+++ app/assets/javascripts/adventure.js
@@ -8,5 +8,9 @@ var scrollToImages = function() {
 
 $(document).ready(function() {
 	$('body').on('click', '#compass a', scrollToImages);
-
+  $('#compass').mouseenter(function(){
+    $(this).css('font-size',70);
+  }).mouseleave(function(){
+    $(this).css('font-size',60);
+  })
 })
