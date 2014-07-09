2e3ea2b936a39a5fe5271bcac5ac5cbb69adab8e
Lasse Sviland <lassesviland@hotmail.com>
2014-07-08 23:36:27 -0400

    fixed seed file

--- db/import_export_csv.rb
+++ db/import_export_csv.rb
@@ -30,7 +30,7 @@ def importGadventures
       cities[csv[2]] = nil
     end 
     p  cities[csv[2]]
-		Adventure.makeAdventure(csv[0],csv[1],csv[2],csv[3],csv[4],csv[5],csv[6],cityBoolean,cities[csv[2]])
+		Adventure.makeAdventure(csv[0],csv[1],csv[2],csv[3],csv[5],csv[4],csv[6],cityBoolean,cities[csv[2]])
 	end
   puts "done importing adventures"
 end
