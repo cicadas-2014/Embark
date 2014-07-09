33866cb9b9d2277740ef9f410ef8480b2ee93eef
Jessica Tatham <jectatham@gmail.com>
2014-07-08 23:13:28 -0400

    Pushing somewhat working code, to overwrite crap code

--- app/controllers/embark_controller.rb
+++ app/controllers/embark_controller.rb
@@ -5,29 +5,33 @@ class EmbarkController < ApplicationController
 	end
 
 	def adventures
-		latitude = params[:latitude]
-		longitude = params[:longitude]
-		search = params[:search]
-		
-		distance = 8 if search == 'local'
-		distance = 20 if search == 'continential'
-		distance = 1000000 if search == 'global'
+		if params[:search] == 'inspire'
+			@adventures = City.all
+		else
+			latitude = params[:latitude]
+			longitude = params[:longitude]
+			search = params[:search]
+			
+			distance = 8 if search == 'local'
+			distance = 20 if search == 'continential'
+			distance = 1000000 if search == 'global'
 
-		valid_adventures = []
-		all_adventures = Adventure.all
-		all_adventures.each do |adv|
-			if adv.city_id
-				city = City.find(adv.city_id)
-				position = [city.latitude, city.longitude]
-				if city.latitude.to_i - latitude.to_i+distance < distance*2 && city.latitude.to_i - latitude.to_i+distance > 0 && city.longitude.to_i - longitude.to_i+distance < distance*2 && city.longitude.to_i - longitude.to_i+distance > 0
-					unless adv.image_url == "nil" || adv.image_url == "Private photo cant use"
-						valid_adventures << adv
+			valid_adventures = []
+			all_adventures = Adventure.all
+			all_adventures.each do |adv|
+				if adv.city_id
+					city = City.find(adv.city_id)
+					position = [city.latitude, city.longitude]
+					if city.latitude.to_i - latitude.to_i+distance < distance*2 && city.latitude.to_i - latitude.to_i+distance > 0 && city.longitude.to_i - longitude.to_i+distance < distance*2 && city.longitude.to_i - longitude.to_i+distance > 0
+						unless adv.image_url == "nil" || adv.image_url == "Private photo cant use"
+							valid_adventures << adv
+						end
 					end
 				end
 			end
+			@categories = Category.all
+			@adventures = valid_adventures.sample(27)
 		end
-		@categories = Category.all
-		@adventures = valid_adventures.sample(27)
 	end
 
 	def description
