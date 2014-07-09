8e9e3519c4a38c11945976b40ca2c8db065954a3
DineshRai <drai89@gmail.com>
2014-07-09 16:05:28 -0400

    add filter category wip

--- app/controllers/embark_controller.rb
+++ app/controllers/embark_controller.rb
@@ -5,6 +5,7 @@ class EmbarkController < ApplicationController
 	end
 
 	def adventures
+		p params
 		if params[:search] == 'inspire'
 			@adventures = []
 			Adventure.all.each do |adv|
@@ -22,10 +23,19 @@ class EmbarkController < ApplicationController
 			distance = 1000000 if search == 'global'
 
 			valid_adventures = []
-			all_adventures = Adventure.all
+			p session
+			if params[:category].nil?
+				all_adventures = Adventure.all
+			else
+				par = JSON.parse(params[:params])
+				p par
+				all_adventures = Category.find(params[:category][:category_id]).adventures
+			end
 			all_adventures.each do |adv|
 				if adv.city_id
 					city = City.find(adv.city_id)
+					p latitude
+					p longitude
 					position = [city.latitude, city.longitude]
 					if city.latitude.to_i - latitude.to_i+distance < distance*2 && city.latitude.to_i - latitude.to_i+distance > 0 && city.longitude.to_i - longitude.to_i+distance < distance*2 && city.longitude.to_i - longitude.to_i+distance > 0
 						unless adv.image_url == "nil" || adv.image_url == "Private photo cant use"
@@ -34,8 +44,8 @@ class EmbarkController < ApplicationController
 					end
 				end
 			end
-			@categories = Category.all
 			@adventures = valid_adventures.sample(27)
+			@params = params
 		end
 	end
 
