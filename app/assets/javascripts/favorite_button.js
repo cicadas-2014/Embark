e5077857f674bea2b30f20b3c7837619ab88a0c1
Jessica Tatham <jectatham@gmail.com>
2014-07-10 17:08:50 -0400

    Working on favorites button

--- app/assets/javascripts/favorite_button.js
+++ app/assets/javascripts/favorite_button.js
@@ -2,7 +2,7 @@ var alterFavoriteButton = function(e) {
   e.preventDefault();
   var data = $('#favorite_button').attr('adventure');
   document.getElementById('favorite_button').value = 'Saved to favorites!!'
-  document.getElementById('favorite_button').adventure = 'nil'
+  // document.getElementById('favorite_button').adventure = 'nil'
   input.disabled = true;
   $.ajax({
     url: "/users/favorites",
